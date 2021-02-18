74c4f0f9b34377d7384f5875174370574d9fb72a
Meng Lu <lumeng.dev@gmail.com>
2021-02-18 15:57:44 -0800

    + Add code to download macaulaylibrary.org images.

--- Scraping.m
+++ Scraping.m
@@ -13,17 +13,98 @@ Begin[ "Private`"];
 ClearAll[Scrape];
 ClearAll[regexData];
 
-
+(******************************************************************************)
 
 regexData["SourceURL", "artron.net"] = RegularExpression["https?://zxp\\.artron\\.net/specials/goods/goodsdetail/[0-9]+"];
 
 regexData["ImageURL", "artron.net"] = RegularExpression["https?://img\\d+\\.artimg\\.net/zxp/auctions/.*/.*\\.jpg"];
 
+regexData["SourceURL", "macaulaylibrary.org"] = RegularExpression["https?://macaulaylibrary\\.org/asset/[0-9]+"];
+
+regexData["ImageURL", "macaulaylibrary.org"] = RegularExpression["https?://img\\d+\\.artimg\\.net/zxp/auctions/.*/.*\\.jpg"];
+
+(******************************************************************************)
+(* macaulaylibrary.org *)
+
 Scrape[
-    dataEntry_Dataset?(StringQ[#[["url"]]] && StringMatchQ[#[["url"]], regexData["SourceURL", "artron.net"]] &),
+    dataEntry_Dataset?(StringQ[#[["url"]]]
+    && StringMatchQ[#[["url"]], regexData["SourceURL", "macaulaylibrary.org"]] &),
     "ImageLinks"
 ] := Composition[
-    {Sort, Identity}[[1]],
+    DeleteDuplicates,
+    Select[
+        #,
+        StringQ[#] && StringMatchQ[#, RegularExpression["https://cdn\\..*"]] &
+    ] &,
+    Import[#, "Hyperlinks"] &,
+    #[["url"]] &
+][dataEntry];
+
+Scrape[
+    dataEntry_Dataset?(StringQ[#[["url"]]]
+    && StringMatchQ[#[["url"]], regexData["SourceURL", "macaulaylibrary.org"]] & )
+] := Composition[
+    With[
+        {urls = #[[2]][[;;]], d = #[[1]]},
+        (
+            Pause[RandomReal[2]];
+            mengDownloadImage[
+                #,
+                "FileBaseName" -> StringJoin[
+                    {
+                        formatFileNameElement[d[["author"]]],
+                        "__",
+                        formatFileNameElement[d[["title"]]],
+                        "_",
+                        IntegerString[First[Position[urls, #]]],
+                        "__",
+                        Which[
+                            DateObjectQ[d[["authoring_date"]]],
+                            DateString[d[["authoring_date"]], "ISODate"],
+                            StringQ[d[["authoring_date"]]],
+                            d[["authoring_date"]],
+                            DateObjectQ[d[["publishing_date"]]],
+                            DateString[d[["publishing_date"]], "ISODate"],
+                            StringQ[d[["publishing_date"]]],
+                            d[["publishing_date"]],
+                            DateObjectQ[d[["retrieving_date"]]],
+                            DateString[d[["retrieving_date"]], "ISODate"],
+                            StringQ[d[["retrieving_date"]]],
+                            d[["retrieving_date"]],
+                            True,
+                            DateString["ISODate"]
+                        ],
+                        "__",
+                        formatFileNameElement[getDomainName[d[["url"]]]],
+                        "_",
+                        formatFileNameElement[d[["publisher"]]],
+                        "_",
+                        FileNameTake[#, -1],
+                        If[
+                            !StringMatchQ[
+                                FileNameTake[#, -1],
+                                RegularExpression[".*\\.\\w{3,}"]
+                            ],
+                            "." <> ToLowerCase@FileFormat[
+                                URLDownload[
+                                    #,
+                                    FileNameJoin[{$TemporaryDirectory, "tmp" <> FileNameTake[#, -1]}]
+                                ]
+                            ],
+                            ""
+                        ]
+                    }
+                ],
+                "Subdirectory" -> {getDomainName[d[["url"]]]},
+                "FileNameRenameRules" -> {
+                    RegularExpression["img\\d+\\.artimg\\.net"] -> "artron.net"
+                }
+            ]
+        ) & /@ urls
+    ] &,
+    {#, Scrape[#, "ImageLinks"]} &
+][dataEntry];
+
 (******************************************************************************)
 (* artron.net *)
 
