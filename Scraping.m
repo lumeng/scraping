163728328d2bb4df7ffc007001cee833c829e878
Meng Lu <lumeng.dev@gmail.com>
2021-02-18 16:37:45 -0800

    !~ Cleaning up; Fixing date formatting.

--- Scraping.m
+++ Scraping.m
@@ -15,13 +15,13 @@ ClearAll[regexData];
 
 (******************************************************************************)
 
-regexData["SourceURL", "artron.net"] = RegularExpression["https?://zxp\\.artron\\.net/specials/goods/goodsdetail/[0-9]+"];
-
-regexData["ImageURL", "artron.net"] = RegularExpression["https?://img\\d+\\.artimg\\.net/zxp/auctions/.*/.*\\.jpg"];
-
 regexData["SourceURL", "macaulaylibrary.org"] = RegularExpression["https?://macaulaylibrary\\.org/asset/[0-9]+"];
 
 regexData["ImageURL", "macaulaylibrary.org"] = RegularExpression["https?://img\\d+\\.artimg\\.net/zxp/auctions/.*/.*\\.jpg"];
+
+regexData["SourceURL", "artron.net"] = RegularExpression["https?://zxp\\.artron\\.net/specials/goods/goodsdetail/[0-9]+"];
+
+regexData["ImageURL", "artron.net"] = RegularExpression["https?://img\\d+\\.artimg\\.net/zxp/auctions/.*/.*\\.jpg"];
 
 (******************************************************************************)
 (* macaulaylibrary.org *)
@@ -141,68 +141,15 @@ Scrape[
                         "__",
                         Which[
                             DateObjectQ[d[["authoring_date"]]],
-                            DateString[d[["authoring_date"]]],
+                            DateString[d[["authoring_date"]], "ISODate"],
                             StringQ[d[["authoring_date"]]],
                             d[["authoring_date"]],
                             DateObjectQ[d[["publishing_date"]]],
-                            DateString[d[["publishing_date"]]],
+                            DateString[d[["publishing_date"]], "ISODate"],
                             StringQ[d[["publishing_date"]]],
                             d[["publishing_date"]],
                             DateObjectQ[d[["retrieving_date"]]],
-                            DateString[d[["retrieving_date"]]],
-                            StringQ[d[["retrieving_date"]]],
-                            d[["retrieving_date"]],
-                            True,
-                            DateString["ISODate"]
-                        ],
-                        "__",
-                        formatFileNameElement[getDomainName[d[["url"]]]],
-                        "_",
-                        formatFileNameElement[d[["publisher"]]],
-                        "_",
-                        FileNameTake[#, -1]
-                    }
-                ],
-                "Subdirectory" -> {getDomainName[d[["url"]]]},
-                "FileNameRenameRules" -> {
-                    RegularExpression["img\\d+\\.artimg\\.net"] -> "artron.net"
-                }
-            ]
-        ) & /@ urls
-    ] &,
-    {#, Scrape[#, "ImageLinks"]} &
-][dataEntry];
-
-
-Scrape[
-    dataEntry_Dataset?(StringQ[#[["url"]]]
-    && StringMatchQ[#[["url"]], regexData["SourceURL", "artron.net"]] & )
-] := Composition[
-    With[
-        {urls = #[[2]][[;;]], d = #[[1]]},
-        (
-            Pause[RandomReal[2]];
-            mengDownloadImage[
-                #,
-                "FileBaseName" -> StringJoin[
-                    {
-                        formatFileNameElement[d[["author"]]],
-                        "__",
-                        formatFileNameElement[d[["title"]]],
-                        "_",
-                        IntegerString[First[Position[urls, #]]],
-                        "__",
-                        Which[
-                            DateObjectQ[d[["authoring_date"]]],
-                            DateString[d[["authoring_date"]]],
-                            StringQ[d[["authoring_date"]]],
-                            d[["authoring_date"]],
-                            DateObjectQ[d[["publishing_date"]]],
-                            DateString[d[["publishing_date"]]],
-                            StringQ[d[["publishing_date"]]],
-                            d[["publishing_date"]],
-                            DateObjectQ[d[["retrieving_date"]]],
-                            DateString[d[["retrieving_date"]]],
+                            DateString[d[["retrieving_date"]], "ISODate"],
                             StringQ[d[["retrieving_date"]]],
                             d[["retrieving_date"]],
                             True,
